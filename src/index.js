2cec07c58d37a7957669ab68d561c0d27b59589b
Ray Zyphs <bruferviveiros@gmail.com>
2020-02-10 21:25:56 -0200

    16. Retornar Múltiplos Elementos

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // import PrimeiroComponente from './components/PrimeiroComponente';
-import {CompA, CompB as B} from './components/DoisComponentes'
+// import {CompA, CompB as B} from './components/DoisComponentes'
+import MultiElementos from './components/MultiElementos'
 
 const elemento = document.getElementById('root');
 ReactDOM.render(
   <div>
-    <CompA valor='Olá eu sou o A!'/>
+    <MultiElementos />
+    {/* <CompA valor='Olá eu sou o A!'/>
     <B valor='B na área'/>
-    {/* <PrimeiroComponente valor='Bom Dia!' /> */}
+    <PrimeiroComponente valor='Bom Dia!' />  */}
   </div>,
   elemento
 );
