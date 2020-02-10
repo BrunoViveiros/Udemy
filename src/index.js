3d98968d87d290ac52225f55ed4b7807897dc4b1
Ray Zyphs <bruferviveiros@gmail.com>
2020-02-10 20:40:23 -0200

    Criado primeiro componente

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import PrimeiroComponente from './components/PrimeiroComponente';
+
+const elemento = document.getElementById('root');
+ReactDOM.render(
+  <div>
+    <PrimeiroComponente></PrimeiroComponente>
+  </div>,
+  elemento
+);
+
+// const jsx = <h1>Olá React</h1>;
+// ReactDOM.render(jsx, elemento);
