a8d7e68aea205afa1ea81f91971b52d6aa336bad
Ray Zyphs <bruferviveiros@gmail.com>
2020-02-11 22:57:41 -0200

    23. Adicionando Função em um Componente Funcional

--- src/index.js
+++ src/index.js
@@ -5,16 +5,18 @@ import ReactDOM from 'react-dom';
 // import CompA, { CompB as B } from './components/DoisComponentes';
 // import MultiElementos from './components/MultiElementos';
 // import FamiliaSilva from './components/FamiliaSilva';
-import Familia from './components/Familia';
-import Membro from './components/Membro';
+// import Familia from './components/Familia';
+// import Membro from './components/Membro';
+import ComponenteComFuncao from './components/ComponenteComFuncao';
 
 const elemento = document.getElementById('root');
 ReactDOM.render(
   <div>
-    <Familia sobrenome='Pereira'>
+    <ComponenteComFuncao />
+    {/* <Familia sobrenome='Pereira'>
       <Membro nome='Andre' />
       <Membro nome='Mariana' />
-    </Familia>
+    </Familia> */}
     {/* <FamiliaSilva /> */}
     {/* <MultiElementos /> */}
     {/* <CompA valor='Olá eu sou o A!' />
