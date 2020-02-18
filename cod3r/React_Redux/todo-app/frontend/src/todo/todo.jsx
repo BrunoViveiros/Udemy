a6173a7886a5942041ca48799a72ca6b8ca9099c
Ray Zyphs <bruferviveiros@gmail.com>
2020-02-18 10:54:08 -0300

    68. Melhorias de CSS e Limpar Formulário

--- cod3r/React_Redux/todo-app/frontend/src/todo/todo.jsx
+++ cod3r/React_Redux/todo-app/frontend/src/todo/todo.jsx
@@ -18,6 +18,7 @@ export default class Todo extends Component {
     this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
     this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
+    this.handleClear = this.handleClear.bind(this);
 
     this.refresh();
   }
@@ -62,15 +63,20 @@ export default class Todo extends Component {
       .then(resp => this.refresh(this.state.description));
   }
 
+  handleClear() {
+    this.refresh();
+  }
+
   render() {
     return (
       <div>
         <PageHeader name='Tarefas' small='Cadastro' />
         <TodoForm
+          description={this.state.description}
           handleAdd={this.handleAdd}
           handleChange={this.handleChange}
           handleSearch={this.handleSearch}
-          description={this.state.description}
+          handleClear={this.handleClear}
         />
         <TodoList
           list={this.state.list}
