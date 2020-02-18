2e681ba62f0a89ef31a83f06e341e31b7f613f5f
Ray Zyphs <bruferviveiros@gmail.com>
2020-02-18 20:06:02 -0300

    Arrumando codigo bagunçado

--- cod3r/React_Redux/exercicios-react/src/index.js
+++ cod3r/React_Redux/exercicios-react/src/index.js
@@ -1,38 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import PrimeiroComponente from './componentes/PrimeiroComponente'
+// import CompA, { CompB as B } from './componentes/DoisComponentes'
+// import MultiElementos from './componentes/MultiElementos'
+// import FamiliaSilva from './componentes/FamiliaSilva'
+// import Familia from './componentes/Familia'
+// import Membro from './componentes/Membro'
+// import ComponenteComFuncao from './componentes/ComponenteComFuncao'
+// import Pai from './componentes/Pai'
+// import ComponenteClasse from './componentes/ComponenteClasse'
+// import Contador from './componentes/Contador'
+import Hook from './componentes/Hook'
 
-// import PrimeiroComponente from './components/PrimeiroComponente';
-// import CompA, { CompB as B } from './components/DoisComponentes';
-// import MultiElementos from './components/MultiElementos';
-// import FamiliaSilva from './components/FamiliaSilva';
-// import Familia from './components/Familia';
-// import Membro from './components/Membro';
-// import ComponenteComFuncao from './components/ComponenteComFuncao';
-// import Pai from './components/Pai';
-// import ComponenteClasse from './components/ComponenteClasse';
-// import Contador from './components/Contador';
-import Hook from './components/Hook';
-
-const elemento = document.getElementById('root');
+const elemento = document.getElementById('root')
 ReactDOM.render(
-  <div>
-    <Hook />
-    {/* <Contador numeroInicial={100} /> */}
-    {/* <ComponenteClasse valor='' /> */}
-    {/* <Pai /> */}
-    {/* <ComponenteComFuncao /> */}
-    {/* <Familia sobrenome='Pereira'>
-      <Membro nome='Andre' />
-      <Membro nome='Mariana' />
-    </Familia> */}
-    {/* <FamiliaSilva /> */}
-    {/* <MultiElementos /> */}
-    {/* <CompA valor='Olá eu sou o A!' />
-    <B valor='B na área' /> */}
-    {/* <PrimeiroComponente valor='Bom Dia!' /> */}
-  </div>,
-  elemento
-);
+    <div>
+        <Hook />
+        {/* <Contador numeroInicial={100} /> */}
+        {/* <ComponenteClasse valor="Sou um componente de classe!" /> */}
+        {/* <Pai /> */}
+        {/* <ComponenteComFuncao /> */}
+        {/* <Familia sobrenome="Pereira">
+            <Membro nome="Andre" />
+            <Membro nome="Mariana" />
+        </Familia> */}
+        {/* <FamiliaSilva /> */}
+        {/* <MultiElementos /> */}
+        {/* <CompA valor="Olá eu sou A!" />
+        <B valor="B na área!" /> */}
+        {/* <PrimeiroComponente valor="Bom dia!" /> */}
+    </div>
+, elemento)
 
-// const jsx = <h1>Olá React</h1>;
-// ReactDOM.render(jsx, elemento);
+
+
+
+// const jsx = <h1>Olá React!</h1>
+// ReactDOM.render(jsx, elemento)
+
+
+// ReactDOM.render(
+//     <ul>
+//         <li>1) Pedro</li>
+//         <li>2) Maria</li>
+//         <li>3) Ana</li>
+//     </ul>
+// , elemento)
+
+// const lista = document.createElement('ul')
+
+// let item = document.createElement('li')
+// let texto = document.createTextNode('1) Pedro')
+// item.appendChild(texto)
+// lista.appendChild(item)
+
+// item = document.createElement('li')
+// texto = document.createTextNode('2) Maria')
+// item.appendChild(texto)
+// lista.appendChild(item)
+
+// item = document.createElement('li')
+// texto = document.createTextNode('3) Ana')
+// item.appendChild(texto)
+// lista.appendChild(item)
+
+// const elemento = document.getElementById('root')
+// elemento.appendChild(lista)
