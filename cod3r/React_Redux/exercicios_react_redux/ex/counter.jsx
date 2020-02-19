98eaa04346d877a9d4d8ccebd96a2eed141526fc
Ray Zyphs <bruferviveiros@gmail.com>
2020-02-19 17:05:05 -0300

    77. Ex 02 - Integrando o React-Redux (Parte 2)

--- cod3r/React_Redux/exercicios_react_redux/ex/counter.jsx
+++ cod3r/React_Redux/exercicios_react_redux/ex/counter.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-const Counter = props => <div>ok</div>;
+import { inc, dec, stepChanged } from './counterAction';
 
-export default Counter;
+const Counter = props => (
+  <div>
+    <h1>{props.counter.number}</h1>
+    <input
+      onChange={props.stepChanged}
+      value={props.counter.step}
+      type='number'
+    />
+    <button onClick={props.dec}>Dec</button>
+    <button onClick={props.inc}>Inc</button>
+  </div>
+);
+const mapStateToProps = state => ({ counter: state.counter });
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ inc, dec, stepChanged }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
