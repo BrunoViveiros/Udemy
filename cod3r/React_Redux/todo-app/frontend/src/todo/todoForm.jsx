1cd7d6fb4022003efa342a2ec5e29d26e9a64439
Ray Zyphs <bruferviveiros@gmail.com>
2020-02-20 21:10:32 -0300

    92. Limpar Formulário

--- cod3r/React_Redux/todo-app/frontend/src/todo/todoForm.jsx
+++ cod3r/React_Redux/todo-app/frontend/src/todo/todoForm.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import { changeDescription, search, add } from './todoActions';
+import { changeDescription, search, add, clear } from './todoActions';
 import Grid from '../template/grid';
 import IconButton from '../template/iconButton';
 
@@ -17,16 +17,16 @@ class TodoForm extends Component {
   }
 
   keyHandler(e) {
-    const { add, search, description } = this.props;
+    const { add, search, description, clear } = this.props;
     if (e.key === 'Enter') {
       e.shiftKey ? search() : add(description);
     } else if (e.key === 'Escape') {
-      this.props.handleClear();
+      clear();
     }
   }
 
   render() {
-    const { add, search, description } = this.props;
+    const { add, search, description, clear } = this.props;
     return (
       <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
@@ -46,11 +46,7 @@ class TodoForm extends Component {
             onClick={() => add(description)}
           />
           <IconButton style='info' icon='search' onClick={() => search()} />
-          <IconButton
-            style='default'
-            icon='close'
-            onClick={this.props.handleClear}
-          />
+          <IconButton style='default' icon='close' onClick={clear} />
         </Grid>
       </div>
     );
@@ -59,6 +55,6 @@ class TodoForm extends Component {
 
 const mapStateToProps = state => ({ description: state.todo.description });
 const mapDispatchToProps = dispatch =>
-  bindActionCreators({ changeDescription, search, add }, dispatch);
+  bindActionCreators({ changeDescription, search, add, clear }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
