cde3a44b364b4ae02cc510a56f7ab5ee08721c5a
Ray Zyphs <bruferviveiros@gmail.com>
2020-02-28 17:21:01 -0300

    127. Integrando Dashboard com Redux (Parte 2)

--- cod3r/React_Redux/my-money-app/frontend/src/index.jsx
+++ cod3r/React_Redux/my-money-app/frontend/src/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-import reducers from './main/reducers';
-import App from './main/app';
+import promise from 'redux-promise';
 
-const store = createStore(reducers);
+import App from './main/app';
+import reducers from './main/reducers';
+
+const store = applyMiddleware(promise)(createStore)(reducers);
 
 ReactDOM.render(
   <Provider store={store}>
