140a70ea433b4961c63b26452fe9b549a410eece
Ray Zyphs <bruferviveiros@gmail.com>
2020-03-14 21:10:07 -0300

    10. Criando um componente e compartilhando entre páginas

---
+++ Willian Justen/Gatsby/my-blog/src/components/Profile/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Bruno Viveiros</h1>
+    <h2>Developer</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid et quis
+      eveniet.
+    </p>
+  </div>
+)
+
+export default Profile
